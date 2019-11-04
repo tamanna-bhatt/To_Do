b4d5b8648ce10ae45eada0ae873a15a4cd16a033
Tamanna <tamannabhatt12@gmail.com>
2019-11-03 17:17:00 -0800

    Fixing the path

--- app/src/main/java/e/wolfsoft1/todo_app/Signup_Todo.java
+++ app/src/main/java/e/wolfsoft1/todo_app/Signup_Todo.java
@@ -1,13 +1,36 @@
 package e.wolfsoft1.todo_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Signup_Todo extends AppCompatActivity {
-
+    TextView txtLogin , btnSignup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup__todo);
+
+        txtLogin = (TextView)findViewById(R.id.txtLogin);
+        btnSignup =(TextView)findViewById(R.id.btnSignup);
+
+        txtLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(Signup_Todo.this,ToDo_login_main.class);
+                startActivity(i);
+            }
+        });
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(Signup_Todo.this,Home_Todo.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/e/wolfsoft1/todo_app/ToDo_login_main.java
+++ app/src/main/java/e/wolfsoft1/todo_app/ToDo_login_main.java
@@ -1,9 +1,14 @@
 package e.wolfsoft1.todo_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ToDo_login_main extends AppCompatActivity {
+
+    TextView txtCreateOne , btnLogin;
 
 
 
@@ -11,6 +16,25 @@ public class ToDo_login_main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.todo_login_main);
+
+        txtCreateOne = (TextView)findViewById(R.id.txtCreateOne);
+        btnLogin =(TextView)findViewById(R.id.btnLogin);
+
+        txtCreateOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i =  new Intent(ToDo_login_main.this ,Signup_Todo.class);
+                startActivity(i);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(ToDo_login_main.this,Home_Todo.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
